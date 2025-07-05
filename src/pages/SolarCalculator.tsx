51317cd36cafda69367192c5c37c3b83a7652f43
subhrajyoutimahanta@gmail.com <subhrajyoutimahanta@gmail.com>
2025-07-06 03:06:27 +0530

    2

--- src/pages/SolarCalculator.tsx
+++ src/pages/SolarCalculator.tsx
@@ -178,7 +178,7 @@ const SolarCalculator: React.FC = () => {
       
       <div className="container mx-auto px-4 py-8">
         {/* Header */}
-        <div className="text-center mb-5 mt-8">
+        <div className="text-center mb-5 mt-2">
           <div className="flex justify-center items-center mb-4">
             <img 
               src="/sunlyticslogo.png" 
