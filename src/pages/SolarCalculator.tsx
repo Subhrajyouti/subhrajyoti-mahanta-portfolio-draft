a944d7b90aa72103805c17e0d374011964affc6b
subhrajyoutimahanta@gmail.com <subhrajyoutimahanta@gmail.com>
2025-07-07 01:06:00 +0530

    200

--- src/pages/SolarCalculator.tsx
+++ src/pages/SolarCalculator.tsx
@@ -116,7 +116,7 @@ const SolarCalculator: React.FC = () => {
     // Step through phases with 10 second intervals
     for (let i=0; i<phases.length-1; i++) {
       setPhaseIndex(i);
-      await new Promise(res => setTimeout(res, 100));
+      await new Promise(res => setTimeout(res, 200));
     }
     
     // Start final phase
